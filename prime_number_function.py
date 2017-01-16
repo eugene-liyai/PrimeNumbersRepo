b48c8758379aa121c8b424500356245880940d7c
eugene-liyai <keugeneliyai@yahoo.com>
2017-01-17 00:47:10 +0300

    implemented prime_number method block and an execution method (get_prime_numbers_in_range) that calls the prime_number function

--- prime_number_function.py
+++ prime_number_function.py
@@ -1,6 +1,18 @@
 def prime_number(n):
-	pass
+	if n == 1:
+		print('1 is a prime number')
+		return False
 
+	for x in range(2, n):
+		if n%x == 0:
+			return False
 
-if __main__ == "__main__":
-	prime_number(1)
+	print n, 'is a prime number'
+	return True
+
+def get_prime_numbers_in_range(n):
+	for x in range(1, n):
+		prime_number(x)
+
+if __name__ == "__main__":
+	get_prime_numbers_in_range(15)
